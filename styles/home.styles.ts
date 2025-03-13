5593286be853e4df6922ef4a3f5c1c0ff42678de
lazari03 <alfonzlazri@yea.al>
2025-03-13 02:37:43 +0100

    wip

--- styles/home.styles.ts
+++ styles/home.styles.ts
@@ -3,54 +3,59 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   headerContainer: {
     backgroundColor: "black",
-    // Removed borderBottomWidth and borderBottomColor
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(255, 255, 255, 0.5)",
   },
   header: {
     flexDirection: "row",
-    justifyContent: 'space-between',
-    backgroundColor: 'black',
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 16,
     paddingVertical: 12,
-    alignItems: 'center',
+    height: 60,
   },
   headerText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 24, // Adjusted font size
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 24,
   },
   headerIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   iconButton: {
     width: 40,
     height: 40,
     justifyContent: "center",
     alignItems: "center",
-    marginLeft: 10, // Adjusted margin
+    marginLeft: 10,
   },
   underline: {
-    height: 2,
-    backgroundColor: 'white',
+    height: 1,
+    backgroundColor: "white",
     marginHorizontal: 16,
-    borderRadius: 1,
-  },
-  sectionTitle: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginTop: 20, // Add marginTop to create space between the title and the featured slider
+    borderRadius: 2,
   },
   stickyHeaderContainer: {
-    backgroundColor: 'black',
-    width: '100%',
+    backgroundColor: "black",
+    width: "100%",
     paddingVertical: 10,
     paddingHorizontal: 16,
   },
+  sectionTitle: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 20,
+  },
   eventListContainer: {
-    width: '100%',
+    width: "100%",
     padding: 16,
-    alignItems: 'center', // Center the event cards
+    alignItems: "center",
   },
 });
+
+export default styles;
